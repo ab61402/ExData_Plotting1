2f17c6542e28a7f5bd74becdc684cc2045753492
ab61402 <ab61402@gmail.com>
2017-06-17 16:17:29 +0800

    Create Plot1.R

---
+++ Plot1.R
@@ -0,0 +1,5 @@
+content <- read.table("SourceData/household_power_consumption.txt",sep = ";",header = T)
+subSetContent <- content[(content$Date=="2/2/2007" | content$Date=="1/2/2007"),]
+hist(as.numeric(subSetContent$Global_active_power), main="Global Active Power", xlab="Global Active Power (kilowatts)", ylab="Frequency", col="Red")
+dev.copy(png, file="plot1.png", height=480, width=480)
+dev.off()
