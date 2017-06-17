2f483bf1121e1681d13f3a2d3541295fae3e4c20
ab61402 <ab61402@gmail.com>
2017-06-17 16:54:27 +0800

    Update and rename Plot3 to Plot3.R

--- Plot3.R
+++ Plot3.R
@@ -1,5 +1,8 @@
 content <- read.table("SourceData/household_power_consumption.txt",sep = ";",header = T)
 subSetContent <- content[(content$Date=="2/2/2007" | content$Date=="1/2/2007"),]
+subSetContent$Date <- as.Date(subSetContent$Date, format="%d/%m/%Y")
+dateTime <- paste(subSetContent$Date, subSetContent$Time)
+subSetContent$DateTime <- as.POSIXct(dateTime)
 with(subSetContent, plot(DateTime, Sub_metering_1, type = "l", xlab = "",ylab = "Energy sub metering"))
 with(subSetContent, points(DateTime, type = "l", Sub_metering_2,col = "red"))
 with(subSetContent, points(DateTime, type = "l", Sub_metering_3,col = "blue"))
