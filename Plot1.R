4fac90f95a839e118bb73a8f20c6b0d769d19bb5
ab61402 <ab61402@gmail.com>
2017-06-17 17:02:15 +0800

    Update comment

--- Plot1.R
+++ Plot1.R
@@ -1,5 +1,12 @@
+# read content
 content <- read.table("SourceData/household_power_consumption.txt",sep = ";",header = T)
+
+# filter
 subSetContent <- content[(content$Date=="2/2/2007" | content$Date=="1/2/2007"),]
+
+#plot
 hist(as.numeric(subSetContent$Global_active_power), main="Global Active Power", xlab="Global Active Power (kilowatts)", ylab="Frequency", col="Red")
+
+#write to png
 dev.copy(png, file="plot1.png", height=480, width=480)
 dev.off()
